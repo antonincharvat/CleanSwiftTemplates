d4d454978e57f5260a9bf46680418bad6b347ab1
Jan Bednář <bednarjanuh@gmail.com>
2021-01-07 12:37:53 +0100

    rebase

---
+++ Clean Swift/Cell.xctemplate/___VARIABLE_sourceCellIdentifier___Cell.swift
@@ -0,0 +1,25 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+final class ___VARIABLE_sourceCellIdentifier___Cell: UITableViewCell {
+
+    // MARK: - Outlets
+
+    //...
+
+    // MARK: - Actions
+}
+
+extension ___VARIABLE_sourceCellIdentifier___Cell: CellConfigurable {
+
+    func configure(with model: ___VARIABLE_sourceCellIdentifier___CellModel) {
+        //...
+    }
+}
---
+++ Clean Swift/Configurator.xctemplate/___FILEBASENAME___Configurator.swift
@@ -0,0 +1,24 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//
+
+import Foundation
+
+struct ___VARIABLE_sceneName___Configurator {
+    
+    static func create(/*name: String*/) -> ___VARIABLE_sceneName___ViewController {
+        let viewController = ___VARIABLE_sceneName___ViewController()  // replace with proper instantiation
+        let presenter = ___VARIABLE_sceneName___Presenter(viewController: viewController)
+        let worker = ___VARIABLE_sceneName___Worker()
+        let interactor = ___VARIABLE_sceneName___Interactor(presenter: presenter, worker: worker/*, name: name*/)
+        let router = ___VARIABLE_sceneName___Router(viewController: viewController, dataStore: interactor)
+        viewController.interactor = interactor
+        viewController.router = router
+        return viewController
+    }
+}
