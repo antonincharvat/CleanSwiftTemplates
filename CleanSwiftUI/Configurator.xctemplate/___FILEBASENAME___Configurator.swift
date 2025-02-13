47d68e29b8095be42d6cd9a013700abc7bebb735
antonincharvat <antonincharvat@gmail.com>
2025-02-13 10:06:02 +0100

    Bunch of updates - worker removal, observable/bindable, preview, cleanup... (#11)

--- CleanSwiftUI/Configurator.xctemplate/___FILEBASENAME___Configurator.swift
+++ CleanSwiftUI/Configurator.xctemplate/___FILEBASENAME___Configurator.swift
@@ -10,13 +10,12 @@
 import Foundation
 
 struct ___VARIABLE_sceneName___Configurator: Configurator {
-
+    
     let middleware: [MiddlewareType] = []
-
+    
     func createView() -> ___VARIABLE_sceneName___View {
         let viewState = ___VARIABLE_sceneName___ViewState()
-        let worker = ___VARIABLE_sceneName___Worker()
-        let interactor = ___VARIABLE_sceneName___Interactor(present: action(to: viewState), dataStore: viewState, worker: worker)
+        let interactor = ___VARIABLE_sceneName___Interactor(present: action(to: viewState), dataStore: viewState)
         let router = ___VARIABLE_sceneName___Router(dataStore: viewState)
         return ___VARIABLE_sceneName___View(viewState: viewState, interactor: interactor, router: router)
     }
--- CleanSwiftUI/View.xctemplate/___FILEBASENAME___View.swift
+++ CleanSwiftUI/View.xctemplate/___FILEBASENAME___View.swift
@@ -10,21 +10,18 @@
 import SwiftUI
 
 struct ___VARIABLE_sceneName___View: View {
-    
-    @ObservedObject var viewState: ___VARIABLE_sceneName___ViewState
+    @Bindable var viewState: ___VARIABLE_sceneName___ViewState
     let interactor: ___VARIABLE_sceneName___Interactor?
     let router: ___VARIABLE_sceneName___Router?
     
     var body: some View {
         Text("___VARIABLE_sceneName___")
-        .onAppear {
-            interactor?.load()
-        }
+            .onAppear {
+                interactor?.load()
+            }
     }
 }
 
-struct ___VARIABLE_sceneName___View_Previews: PreviewProvider {
-    static var previews: some View {
-        ___VARIABLE_sceneName___View(viewState: ___VARIABLE_sceneName___ViewState(), interactor: nil, router: nil)
-    }
+#Preview {
+    ___VARIABLE_sceneName___View(viewState: ___VARIABLE_sceneName___ViewState(), interactor: nil, router: nil)
 }
